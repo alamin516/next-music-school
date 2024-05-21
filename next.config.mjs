af58c7af9f56dd7f1dfd61d521328f4d9c68bbeb
Md Alamin <alaminduc516@gmail.com>
2024-05-22 04:13:40 +0600

    updated postcss.config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPlugins from 'next-compose-plugins';
 
-export default nextConfig;
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+};
+
+export default withPlugins([], nextConfig);
+
